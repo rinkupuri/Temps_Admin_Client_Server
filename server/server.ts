f13fb7a94e915536693242d2e00b8f3825ce22ab
rinkupuri <rinkupuri124@gmail.com>
2024-07-24 08:57:39 +0530

    origin update

--- server/server.ts
+++ server/server.ts
@@ -11,7 +11,7 @@ const app = express();
 // All milddlewares
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: ["http://localhost:3000", "https://temps-admin.vercel.app"],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
   })
