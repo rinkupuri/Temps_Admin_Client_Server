e7970d8508f23ab647cd072a8c9029f9fc46a9a1
rinkupuri <rinkupuri124@gmail.com>
2024-10-08 17:04:54 +0530

    Routes Fix

--- client/providers/ProtectedRoute.tsx
+++ client/providers/ProtectedRoute.tsx
@@ -2,21 +2,53 @@
 import { useGetUserQuery } from "@/Redux/RTK/auth.api";
 import { setUser } from "@/Redux/State/api.slice";
 import { RootState } from "@/Redux/Store";
-import { useRouter } from "next/navigation";
+import { usePathname, useRouter } from "next/navigation";
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 
 const ProtectedRoute = ({ children }: { children: React.ReactNode }) => {
-  const { data, isLoading, status, error } = useGetUserQuery({});
-  const { user } = useSelector((state: RootState) => state);
+  const pathname = usePathname();
+  const { data, isLoading, isError } = useGetUserQuery({});
+  const user = useSelector((state: RootState) => state.user);
   const dispatch = useDispatch();
   const router = useRouter();
+
   useEffect(() => {
-    if (error && !data && status === "rejected") router.push("/login");
-    if (data && !user && status === "fulfilled") dispatch(setUser(data));
-  }, [data, isLoading, status]);
+    // Redirect to login if there's an error and no data
+    if (isError && !data) {
+      router.push("/login");
+      return;
+    }
+
+    // Set the user data if we have fetched it but it's not in the Redux store
+    if (data && !user && !isLoading) {
+      dispatch(setUser(data));
+    }
+
+    // Check for user permissions on the current path
+    if (user?.user) {
+      const hasPermission = user.user.Permission?.includes(
+        pathname.replace("/", "")
+      );
+
+      // Redirect to home if user does not have permission for the current path
+      if (!hasPermission && pathname !== "/") {
+        router.push("/");
+      }
+    }
+  }, [data, dispatch, isError, isLoading, pathname, router, user]);
+
+  // Render children only if user has permission
+  const hasPermission = user?.user?.Permission?.includes(
+    pathname.replace("/", "")
+  );
+
   return (
-    <>{!isLoading && data && status !== "rejected" && <div>{children}</div>}</>
+    <>
+      {!isLoading && data && !isError && (hasPermission || pathname === "/") ? (
+        <div>{children}</div>
+      ) : null}
+    </>
   );
 };
 
