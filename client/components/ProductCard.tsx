519e6cc4f7036f2098bbd2b061f9f16e9fffa8d5
rinkupuri <rinkupuri124@gmail.com>
2025-04-11 15:36:11 +0530

    Product Card Changed

--- client/components/ProductCard.tsx
+++ client/components/ProductCard.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @next/next/no-img-element */
 import React, { FC, useEffect, useState } from "react";
 import { cart, Product } from "@/types/ProductCardTypes";
 import { usePathname } from "next/navigation";
